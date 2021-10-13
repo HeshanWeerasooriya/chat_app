4ce9c665cb487eb5d8454649b49c3a7ff10a74e2
Heshan <heshan.imedha@gmail.com>
2021-10-13 21:12:33 +0530

    Showing Messages

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -1,16 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('chats').snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(8.0),
-          child: Text('This Works!'),
+      body: SafeArea(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: _usersStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
+
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                return Text(data['text']);
+              }).toList(),
+            );
+          },
         ),
       ),
     );
