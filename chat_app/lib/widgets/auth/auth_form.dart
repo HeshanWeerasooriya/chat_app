2b42e87bb40ce438d209e93e582a9e395e3bd507
Heshan <heshan.imedha@gmail.com>
2021-10-14 07:17:48 +0530

    Changing auth form according to user loged or not

--- chat_app/lib/widgets/auth/auth_form.dart
+++ chat_app/lib/widgets/auth/auth_form.dart
@@ -9,9 +9,10 @@ class AuthForm extends StatefulWidget {
 
 class _AuthFormState extends State<AuthForm> {
   final _formKey = GlobalKey<FormState>();
-  String _userEmail = '';
-  String _userName = '';
-  String _userPassword = '';
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
 
   void _trySubmit() {
     final isValid = _formKey.currentState!.validate();
@@ -39,6 +40,7 @@ class _AuthFormState extends State<AuthForm> {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: ValueKey('email'),
                     validator: (value) {
                       if (value!.isEmpty || !value.contains('@')) {
                         return 'Please enter a valid email address';
@@ -53,21 +55,24 @@ class _AuthFormState extends State<AuthForm> {
                       _userEmail = value!;
                     },
                   ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty || value.length < 4) {
-                        return 'Please enter at least 4 characers';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Username',
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('username'),
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 characers';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                      ),
+                      onSaved: (value) {
+                        _userName = value!;
+                      },
                     ),
-                    onSaved: (value) {
-                      _userName = value!;
-                    },
-                  ),
                   TextFormField(
+                    key: ValueKey('password'),
                     validator: (value) {
                       if (value!.isEmpty || value.length < 7) {
                         return 'Password must be least at 7 characers long';
@@ -84,12 +89,18 @@ class _AuthFormState extends State<AuthForm> {
                   ),
                   SizedBox(height: 12),
                   ElevatedButton(
-                    child: Text('Login'),
+                    child: Text(_isLogin ? 'Login' : 'Singup'),
                     onPressed: _trySubmit,
                   ),
                   TextButton(
-                    child: Text('Create new account'),
-                    onPressed: () {},
+                    child: Text(_isLogin
+                        ? 'Create new account'
+                        : 'I already have an account'),
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
                   )
                 ],
               ),
