d875c3a2b5c436bf6715ff1d52a063d73cfc5266
Heshan <heshan.imedha@gmail.com>
2021-10-14 06:58:16 +0530

    Login Form Validated data printing

--- chat_app/lib/widgets/auth/auth_form.dart
+++ chat_app/lib/widgets/auth/auth_form.dart
@@ -8,6 +8,23 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState!.save();
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,30 +34,58 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       labelText: 'Email address',
                     ),
+                    onSaved: (value) {
+                      _userEmail = value!;
+                    },
                   ),
                   TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 4) {
+                        return 'Please enter at least 4 characers';
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(
                       labelText: 'Username',
                     ),
+                    onSaved: (value) {
+                      _userName = value!;
+                    },
                   ),
                   TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 7) {
+                        return 'Password must be least at 7 characers long';
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(
                       labelText: 'Password',
                     ),
                     obscureText: true,
+                    onSaved: (value) {
+                      _userPassword = value!;
+                    },
                   ),
                   SizedBox(height: 12),
                   ElevatedButton(
                     child: Text('Login'),
-                    onPressed: () {},
+                    onPressed: _trySubmit,
                   ),
                   TextButton(
                     child: Text('Create new account'),
